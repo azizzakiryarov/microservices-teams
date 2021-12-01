d03d602ffa3114522885927373c193f1bb5c86ba
azizzakiryarov <azizzakiryarov87@gmail.com>
2021-12-01 18:05:19 +0100

    Har lagt shell command för att kunna ta bort och bygga deployment samt service

---
+++ script/local/microservices-teams/create-deployment-microservices-teams.sh
@@ -0,0 +1,3 @@
+kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-teams/k8s/base/microservices-teams/deployment.yaml
+
+kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-teams/k8s/base/microservices-teams/service.yaml
